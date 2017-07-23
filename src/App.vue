f1904a368d576245294a184947a569a7bce79d81
James Sullivan <james@scelos.com>
2017-07-23 15:25:37 -0400

    Initial Resume Section done

--- src/App.vue
+++ src/App.vue
@@ -44,22 +44,30 @@ a {
 }
 
 .material-container {
+  &::first-child {
+    margin-top: 20px;
+  }
   background-color: #FFFFFF;
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-  height: 500px;
   font-family: 'Lato', sans-serif;
   margin: 10px;
   text-align: left;
   border: 15px solid transparent;
-  
-  .title {
-    font-size: 32pt;
-    font-weight: 500;
+
+  .title-box {
+    padding-bottom: 20px;
+    .title {
+      font-size: 32pt;
+      font-weight: 500;
+    }
+    .sub-title {
+      font-size: 15pt;
+      font-weight: 300;
+      padding-left: 2pt;
+    }
+
   }
-  .font-size {
-    font-size: 24pt;
-    font-weight: 300;
-  }
+
 
 }
 
--- src/components/sections/Resume.vue
+++ src/components/sections/Resume.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="resume" class="content-section">
-    <div class="material-container"></div>
+    <div class="material-container">
+      <div class="title-box">
+        <div class="title">Resume</div>
+        <div class="sub-title">PDF Download</div>
+      </div>
+      <div class="download-grid">
+        <div onClick="window.location='/downloads/Resume.pdf'; return false;" class="material-container download-box">
+          <i class="fa fa-download" aria-hidden="true"></i>
+          <span class="download-text"> Resume.pdf</span>
+      </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,4 +27,23 @@ export default {
 </script>
 
 <style lang="scss">
+.download-grid {
+  display: grid;
+  grid-template-columns: 2fr 2fr 2fr;
+  .download-box {
+    grid-column: 2;
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+    &:hover {
+      box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    }
+    &:active {
+      box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    }
+    .download-text {
+      padding-left: 15px;
+    }
+  }
+
+}
+
 </style>
