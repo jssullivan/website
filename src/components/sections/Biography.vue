30849f049a8ef57f1d2461a788910a2d7e9a802c
James Sullivan <james@scelos.com>
2017-07-24 01:37:54 -0400

    Made Images larger

--- src/components/sections/Biography.vue
+++ src/components/sections/Biography.vue
@@ -36,8 +36,8 @@ export default {
         background-size: cover;
         box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 
-        height: 200px;
-        width: 300px;
+        height: 300px;
+        width: 400px;
         border-radius: 3px;
     }
   }
